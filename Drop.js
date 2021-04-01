cb62ed38905b11d559a8a77e2b3129156c99fe20
Rakshushamin <75742018+Rakshushamin@users.noreply.github.com>
2021-04-01 22:19:03 +0530

    Add files via upload

--- Drop.js
+++ Drop.js
@@ -24,11 +24,11 @@ class Drop{
       }
       display()
       {
-        var maxDrops=100;
-        for(var i=0;i<maxDrops;i++)
-        {
-             drops.push(new createDrop(random(0,400),random(0,400)));
-        }
+        //var maxDrops=100;
+        //for(var i=0;i<maxDrops;i++)
+       // {
+        //     drops.push(new createDrop(random(0,400),random(0,400)));
+       // }
 
         var pos =this.body.position;
         //pos.x=mouseX;
